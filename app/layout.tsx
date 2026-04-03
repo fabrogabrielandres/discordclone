9215f902f18912c40d16ae0b1ea9588e57e14b11
GABIFABRO\fabro <fabrogabrielandres@gmail.com>
2026-04-03 22:54:13 +0200

    feat: add Clerk authentication middleware and update dependencies
    - Added @clerk/nextjs for authentication support.
    - Introduced proxy.ts to handle authentication middleware for public and protected routes.
    - Updated package.json to include stream-chat and stream-chat-react dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/Components/Providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {/* <Providers> */}
+        <ClerkProvider>
+
+          {children}
+        </ClerkProvider>
+        {/* </Providers> */}
+      </body>
     </html>
   );
 }
